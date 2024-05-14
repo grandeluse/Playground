1137b42a4e316e3b27a7c314525a5bd9aac575b7
grandeluse <grandeluse@gmail.com>
2024-05-14 23:21:56 +0200

    Added Unit Test project

---
+++ Calculator.Tests.Unit/MyCalculatorTest.cs
@@ -0,0 +1,17 @@
+﻿namespace Calculator.Tests.Unit;
+
+public class MyCalculatorTest
+{
+    private readonly MyCalculator _sut;
+
+    public MyCalculatorTest()
+    {
+        _sut = new();
+    }
+
+    [Fact]
+    public void MyCalculatorShouldStart()
+    {
+        _sut.Start();
+    }
+}
