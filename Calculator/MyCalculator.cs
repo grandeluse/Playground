92c3d6e8df97ad56409fae47b3a1673732692872
grandeluse <grandeluse@gmail.com>
2024-05-29 23:00:14 +0200

    [LN-01] docs: test fetch operation

--- Calculator/MyCalculator.cs
+++ Calculator/MyCalculator.cs
@@ -13,8 +13,6 @@ public class MyCalculator(IConsoleManager console) : ICalculator
         {"+","[+] sum" },
         {"/","[/] division" },
         {"ESC", "[ESC] exit" }
-        
-        
     };
 
     public void Start()
