0052df1e8a50261b091156b0c8fa2cf21bc37597
grandeluse <grandeluse@gmail.com>
2024-05-30 16:15:47 +0200

    [LN-01] feat: dynamically registration IOperation implementation

---
+++ Calculator/Infrastructure/RegisterOperations.cs
@@ -0,0 +1,20 @@
+using Calculator.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Calculator.Infrastructure;
+
+public static class RegisterOperations
+{
+    public static IServiceCollection AddOperations(this IServiceCollection serviceCollection)
+    {
+        var operations = typeof(IOperation).Assembly
+            .GetTypes()
+            .Where(type => type.GetInterfaces()
+                .Contains(typeof(IOperation)))
+            .ToList();
+        
+        operations.ForEach(operation=> serviceCollection.AddTransient(typeof(IOperation),operation));
+
+        return serviceCollection;
+    }
+}
