56bed578a273e87e35c276ba59c2d28dd750b56f
grandeluse <grandeluse@gmail.com>
2024-06-01 12:20:26 +0200

    [LN-03] - Benchmark example

---
+++ RandomHack/LoopPerformance.cs
@@ -0,0 +1,46 @@
+using BenchmarkDotNet.Attributes;
+using Microsoft.Diagnostics.Tracing.Parsers.MicrosoftWindowsTCPIP;
+
+namespace RandomHack;
+
+[MemoryDiagnoser(false)]
+public class LoopPerformance
+{
+    private const int Size = 1_000_000;
+    private readonly string[] _list = new string[Size];
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        var random = new Random(420);
+        for (var i = 0; i < Size; i++)
+        {
+            _list[i] = random.Next().ToString();
+        }
+    }
+
+    [Benchmark]
+    public string For()
+    {
+        var result = string.Empty;
+        var size = _list.Length;
+        for (var i = 0; i < size; i++)
+        {
+            result = _list[i];
+        }
+
+        return result;
+    }
+
+    [Benchmark]
+    public string Foreach()
+    {
+        var result = string.Empty;
+        foreach (var item in _list)
+        {
+            return result;
+        }
+
+        return result;
+    }
+}
